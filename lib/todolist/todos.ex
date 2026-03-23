a90781197288ac70c9bcfe6eb8a96b8809f1b945
pomv <taehodev@gmail.com>
2026-03-24 00:49:42 +0900

    테이블/스키마 추가, 컨텍스트 파일 작성

---
+++ lib/todolist/todos.ex
@@ -0,0 +1,31 @@
+defmodule Todolist.Todos do
+  import Ecto.Query, warn: false
+  alias Todolist.Repo
+  alias Todolist.Todos.Todo
+
+  def list_todos do
+    Repo.all(Todo)
+  end
+
+  def get_todo!(id), do: Repo.get!(Todo, id)
+
+  def create_todo(attrs \\ %{}) do
+    %Todo{}
+    |> Todo.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_todo(%Todo{} = todo, attrs) do
+    todo
+    |> Todo.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_todo(%Todo{} = todo) do
+    Repo.delete(todo)
+  end
+
+  def change_todo(%Todo{} = todo, attrs \\ %{}) do
+    Todo.changeset(todo, attrs)
+  end
+end
